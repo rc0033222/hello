8dbab72f4eb094ff1016b63ffe24f0cb01b6df55
Caine Atkinson <rc0033222@student.riversidecollege.ac.uk>
2018-09-10 15:29:38 +0100

    buggged

--- hello/Form1.cs
+++ hello/Form1.cs
@@ -18,8 +18,8 @@ namespace hello
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("hello");
-        }
+        (
+            messagebox,show<'hello'>:
+        )
     }
 }
